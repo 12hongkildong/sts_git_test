e77d02371855f924a4855616ccccaf9e5f51c408
12hongkildong <12hongkildong@gmail.com>
2023-10-16 22:34:46 +0900

    왜 깃 허브의 커밋 메시지가 하나씩 밀리는거지?

--- demo/src/main/java/com/example/demo/GitHubTest.java
+++ demo/src/main/java/com/example/demo/GitHubTest.java
@@ -5,6 +5,7 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("sts로 깃허브 올려보기 연습");
 		System.out.println("아니 이거 또 왜 잔디 안 찍히는 건데");
+		System.out.println("왜 커밋 메시지가 하나씩 밀리는 거지?");
 
 	}
 
