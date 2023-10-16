f755cc89f839b15edd953c39491aba95fbee2781
12hongkildong <12hongkildong@gmail.com>
2023-10-16 22:38:43 +0900

    이메일과 네임 등록 후 커밋 해보기

--- demo/src/main/java/com/example/demo/GitHubTest.java
+++ demo/src/main/java/com/example/demo/GitHubTest.java
@@ -7,6 +7,7 @@ public class GitHubTest {
 		System.out.println("아니 이거 또 왜 잔디 안 찍히는 건데");
 		System.out.println("왜 커밋 메시지가 하나씩 밀리는 거지?");
 		System.out.println("다른 사람 이름으로 올리면 커밋 수가 안 오르나?");
+		System.out.println("유저네임 이메일 설정 후 커밋 테느트");
 	}
 
 }
