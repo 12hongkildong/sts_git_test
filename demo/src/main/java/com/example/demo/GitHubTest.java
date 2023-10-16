b691598fbee24f5c8fb9421c31974eac5593c5fb
slth <slth@host.docker.internal>
2023-10-16 22:35:48 +0900

    커밋수 확인 푸시

--- demo/src/main/java/com/example/demo/GitHubTest.java
+++ demo/src/main/java/com/example/demo/GitHubTest.java
@@ -6,7 +6,7 @@ public class GitHubTest {
 		System.out.println("sts로 깃허브 올려보기 연습");
 		System.out.println("아니 이거 또 왜 잔디 안 찍히는 건데");
 		System.out.println("왜 커밋 메시지가 하나씩 밀리는 거지?");
-
+		System.out.println("다른 사람 이름으로 올리면 커밋 수가 안 오르나?");
 	}
 
 }
